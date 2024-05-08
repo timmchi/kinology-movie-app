d9f274abf2ab510f58d7b0a9ade3632e703b37b6
timmchi <tnmagg@gmail.com>
2024-05-08 12:32:27 +0300

    single movie path added to backend

--- kinology_backend/controllers/movies.js
+++ kinology_backend/controllers/movies.js
@@ -5,8 +5,12 @@ const movieUtils = require("../utils/movieUtils");
 
 const baseMovieUrl =
   "https://api.themoviedb.org/3/discover/movie?include_adult=false&language=en-US&";
+
+const baseSingleMovieUrl = "https://api.themoviedb.org/3/movie/";
+
 const basePersonUrl =
   "https://api.themoviedb.org/3/search/person?include_adult=false&page=1&";
+
 const authorizationHeader = `Authorization: Bearer ${config.TMDB_TOKEN}`;
 const headers = {
   accept: "application/json",
@@ -48,6 +52,30 @@ moviesRouter.post("/", async (request, response) => {
   }));
 
   response.status(200).send({ movieToFrontObjectArray });
+});
+
+moviesRouter.get("/:id", async (request, response) => {
+  const { id } = request.params;
+
+  const url = `${baseSingleMovieUrl}${id}?language=en-US`;
+
+  const movieResponse = await axios.get(url, { headers });
+
+  const movieResults = movieResponse.data;
+
+  const movieObject = {
+    image: movieResults.poster_path,
+    genres: movieResults.genres,
+    overview: movieResults.overview,
+    release: movieResults.release_date,
+    runtime: movieResults.runtime,
+    title: movieResults.title,
+    slogan: movieResults.tagline,
+    rating: movieResults.vote_average,
+    imdb: movieResults.imdb_id,
+  };
+
+  response.status(200).send(movieObject);
 });
 
 module.exports = moviesRouter;
