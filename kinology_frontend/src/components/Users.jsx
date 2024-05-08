38fcdc5dc4a6287bc90b513164717ad273719675
timmchi <tnmagg@gmail.com>
2024-05-08 15:13:41 +0300

    styles for user list

--- kinology_frontend/src/components/Users.jsx
+++ kinology_frontend/src/components/Users.jsx
@@ -1,5 +1,13 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import usersService from "../services/users";
+
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
@@ -12,12 +20,41 @@ const Users = () => {
 
     fetchUsers();
   }, []);
+
+  console.log(users);
   return (
     <div>
       <h1>users</h1>
-      {users?.map((user) => (
-        <p key={user.id}>{user.name}</p>
-      ))}
+      <List
+        sx={{
+          width: "100%",
+          backgroundColor: "#2F764F",
+          padding: 5,
+        }}
+        className="userList"
+      >
+        {users?.map((user) => (
+          <ListItem
+            alignItems="flex-start"
+            key={user.id}
+            sx={{
+              backgroundColor: "#79C094",
+              color: "#fff",
+              borderRadius: 10,
+            }}
+            component={Link}
+            to={`/users/${user.id}`}
+          >
+            <ListItemAvatar>
+              <Avatar alt={user.name} src={user.avatar}></Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary={user.name}
+              secondary="Favorite movie: Godfather II"
+            />
+          </ListItem>
+        ))}
+      </List>
     </div>
   );
 };
