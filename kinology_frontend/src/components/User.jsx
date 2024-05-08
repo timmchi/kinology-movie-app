9517ad080178b80f940edce14923d1befaaeefd4
timmchi <tnmagg@gmail.com>
2024-05-08 15:31:03 +0300

    basic outline of indivial user view added

--- kinology_frontend/src/components/User.jsx
+++ kinology_frontend/src/components/User.jsx
@@ -18,7 +18,38 @@ const User = () => {
   return (
     <div>
       <h1>User</h1>
-      <p>{user.name}</p>
+      <p>
+        <strong>{user.name}</strong>
+      </p>
+      <img src={user.avatar} alt="user avatar" />
+      <div>
+        <h2>About me</h2>
+        <p>{user.biography}</p>
+      </div>
+      <div>
+        <h3>favorite movies</h3>
+        <ul>
+          {user?.favoriteMovies?.map((movie) => (
+            <li key={movie.id}>{movie.title}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h3>watched movies</h3>
+        <ul>
+          {user?.watchedMovies?.map((movie) => (
+            <li key={movie.id}>{movie.title}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>comments</h2>
+        <ul>
+          {user?.profileComments?.map((comment) => (
+            <li key={comment.id}>{comment.content}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
