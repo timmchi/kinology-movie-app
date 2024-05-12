303a9e4a6750eca56492583ea8a9cf9e17b29ecd
timmchi <tnmagg@gmail.com>
2024-05-12 13:01:03 +0300

    hook form and validation for comments

--- kinology_frontend/src/components/CommentForm.jsx
+++ kinology_frontend/src/components/CommentForm.jsx
@@ -1,29 +1,50 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { valibotResolver } from "@hookform/resolvers/valibot";
+import { object, string, minLength } from "valibot";
+
+const CommentSchema = object({
+  content: string("Comment must be a string", [
+    minLength(1, "Comments can not be empty"),
+  ]),
+});
 
 const CommentForm = ({ commentAction, commentId, authorId }) => {
-  const [content, setContent] = useState("");
+  //   const [content, setContent] = useState("");
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
+  } = useForm({
+    resolver: valibotResolver(CommentSchema),
+  });
 
-  const submitComment = (event) => {
-    event.preventDefault();
-    console.log("authorId in submitComment in CommentForm", authorId);
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset({
+        content: "",
+      });
+    }
+  }, [isSubmitSuccessful, reset]);
+
+  // comments are created and edited in the same way. On creation, commentId and authorId are not used, but on edit, they are
+  const submitComment = async ({ content }) => {
+    console.log(content);
     commentAction(content, commentId, authorId);
-    setContent("");
   };
 
   return (
-    <div>
-      <p>comment form</p>
-      <form onSubmit={submitComment}>
-        <div>
-          <input
-            onChange={({ target }) => setContent(target.value)}
-            placeholder="Your comment..."
-            value={content}
-          />
-        </div>
-        <button type="submit">submit comment</button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit(submitComment)}>
+      <div>
+        Your comment
+        <input {...register("content")} />
+        <p style={{ color: "red" }}>{errors.content?.message}</p>
+      </div>
+      <button disabled={isSubmitting} type="submit">
+        {isSubmitting ? "Commenting..." : "Submit comment"}
+      </button>
+    </form>
   );
 };
 
