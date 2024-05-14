9f9dc552b7b55f0fe1e1c02404927274938015cb
timmchi <tnmagg@gmail.com>
2024-05-14 14:40:27 +0300

    users list refactored, single user in list component added, users in user list now have an avatar that works

---
+++ kinology_frontend/src/components/UserSingleListView.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import usersService from "../services/users";
+
+const UserSingleListView = ({ user }) => {
+  const [avatar, setAvatar] = useState("");
+
+  useEffect(() => {
+    const fetchAvatar = async () => {
+      const userAvatar = await usersService.getUserAvatar(user.id);
+      setAvatar(userAvatar);
+    };
+    fetchAvatar();
+  }, [user.id]);
+
+  return (
+    <ListItem
+      alignItems="flex-start"
+      sx={{
+        backgroundColor: "#79C094",
+        color: "#fff",
+        borderRadius: 10,
+      }}
+      component={Link}
+      to={`/users/${user.id}`}
+    >
+      <ListItemAvatar>
+        <Avatar alt={user?.name} src={avatar} />
+      </ListItemAvatar>
+      <ListItemText
+        primary={user.name}
+        secondary="Favorite movie: Godfather II"
+      />
+    </ListItem>
+  );
+};
+
+export default UserSingleListView;
--- kinology_frontend/src/components/Users.jsx
+++ kinology_frontend/src/components/Users.jsx
@@ -4,6 +4,7 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import Divider from "@mui/material/Divider";
 import ListItemText from "@mui/material/ListItemText";
+import UserSingleListView from "./UserSingleListView";
 
 // TODO - fix the state so that on profile update user information in user list changes
 const Users = ({ users }) => {
@@ -21,21 +22,7 @@ const Users = ({ users }) => {
       >
         {users?.map((user) => (
           <div key={user.id}>
-            <ListItem
-              alignItems="flex-start"
-              sx={{
-                backgroundColor: "#79C094",
-                color: "#fff",
-                borderRadius: 10,
-              }}
-              component={Link}
-              to={`/users/${user.id}`}
-            >
-              <ListItemText
-                primary={user.name}
-                secondary="Favorite movie: Godfather II"
-              />
-            </ListItem>
+            <UserSingleListView user={user} />
             <Divider variant="inset" component="li" />
           </div>
         ))}
