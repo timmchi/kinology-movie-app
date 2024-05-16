31d4cad44a8a69ee6b3a50a8c6fb9ba2bf371dd6
timmchi <tnmagg@gmail.com>
2024-05-16 13:12:44 +0300

    more tests added

--- kinology_backend/tests/comments_api.test.js
+++ kinology_backend/tests/comments_api.test.js
@@ -1,8 +1,10 @@
 const { test, after, beforeEach } = require("node:test");
 const assert = require("node:assert/strict");
 const UserComment = require("../models/userComment");
+const User = require("../models/user");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+const bcrypt = require("bcrypt");
 const app = require("../app");
 
 const api = supertest(app);
@@ -14,6 +16,18 @@ const initialComments = [
   { content: "This user is my enemy" },
 ];
 
+const getHash = async () => {
+  const testPasswordHash = await bcrypt.hash("123456", 10);
+  return testPasswordHash;
+};
+
+const initialUser = {
+  username: "commentstester",
+  email: "commentstester@example.com",
+};
+
+let token;
+let receiverId;
 beforeEach(async () => {
   await UserComment.deleteMany({});
   let commentObject;
@@ -21,7 +35,29 @@ beforeEach(async () => {
     commentObject = new UserComment(comment);
     await commentObject.save();
   });
+
+  // creating a user
+  await User.deleteMany({});
+  const passwordHash = await getHash();
+  const user = new User({
+    username: initialUser.username,
+    email: initialUser.email,
+    passwordHash,
+  });
+  await user.save();
+  receiverId = user._id.toString();
+
+  // logging in and getting a token
+  const result = await api
+    .post("/api/login")
+    .send({ username: "commentstester", password: "123456" });
+
+  token = result.body.token;
 });
+
+// npm test -- tests/note_api.test.js
+// npm test -- --test-name-pattern="the first note is about HTTP methods"
+// npm run test -- --test-name-pattern="notes"
 
 test("comments are returned as json", async () => {
   await api
@@ -30,10 +66,38 @@ test("comments are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 });
 
-test("there are 4 comments", async () => {
+test("there is a correct amount of comments", async () => {
   const response = await api.get("/api/comments");
 
   assert.strictEqual(response.body.length, initialComments.length);
+});
+
+test("there is a comment about a great movie", async () => {
+  const response = await api.get("/api/comments");
+
+  const contents = response.body.map((c) => c.content);
+  assert(contents.includes("This is a great movie"));
+});
+
+test("a valid comment can be added to a user profile", async () => {
+  const newComment = {
+    content: "I am a comment created by a test user",
+  };
+
+  await api
+    .post(`/api/comments/profile/${receiverId}`)
+    .set("Authorization", `Bearer ${token}`)
+    .send(newComment)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/comments");
+
+  const contents = response.body.map((c) => c.content);
+
+  assert.strictEqual(response.body.length, initialComments.length + 1);
+
+  assert(contents.includes("I am a comment created by a test user"));
 });
 
 after(async () => {
