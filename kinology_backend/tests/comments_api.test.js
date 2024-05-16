437a506e3e8ea7c0a3f8da88824f4e7142e7b4ec
timmchi <tnmagg@gmail.com>
2024-05-16 16:25:49 +0300

    comment api tests structured better

--- kinology_backend/tests/comments_api.test.js
+++ kinology_backend/tests/comments_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require("node:test");
+const { test, after, beforeEach, describe } = require("node:test");
 const assert = require("node:assert/strict");
 const UserComment = require("../models/userComment");
 const User = require("../models/user");
@@ -10,8 +10,8 @@ const helper = require("./test_helper");
 
 const api = supertest(app);
 
-const getHash = async () => {
-  const testPasswordHash = await bcrypt.hash("123456", 10);
+const getHash = async (pw) => {
+  const testPasswordHash = await bcrypt.hash(pw, 10);
   return testPasswordHash;
 };
 
@@ -19,15 +19,16 @@ let token;
 let receiverId;
 beforeEach(async () => {
   await UserComment.deleteMany({});
-  let commentObject;
-  helper.initialComments.forEach(async (comment) => {
-    commentObject = new UserComment(comment);
-    await commentObject.save();
-  });
+
+  const commentObjects = helper.initialComments.map(
+    (comment) => new UserComment(comment)
+  );
+  const promiseArray = commentObjects.map((comment) => comment.save());
+  await Promise.all(promiseArray);
 
   // creating a user
   await User.deleteMany({});
-  const passwordHash = await getHash();
+  const passwordHash = await getHash("123456");
   const user = new User({
     username: helper.initialUser.username,
     email: helper.initialUser.email,
@@ -48,110 +49,160 @@ beforeEach(async () => {
 // npm test -- --test-name-pattern="the first note is about HTTP methods"
 // npm run test -- --test-name-pattern="notes"
 
-test("comments are returned as json", async () => {
-  await api
-    .get("/api/comments")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("when there are comments in the db", () => {
+  beforeEach(async () => {
+    await UserComment.deleteMany({});
+
+    const commentObjects = helper.initialComments.map(
+      (comment) => new UserComment(comment)
+    );
+    const promiseArray = commentObjects.map((comment) => comment.save());
+    await Promise.all(promiseArray);
+  });
+
+  test("comments are returned as json", async () => {
+    await api
+      .get("/api/comments")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("there is a correct amount of comments", async () => {
+    const commentsAtEnd = await helper.commentsInDb();
+
+    assert.strictEqual(commentsAtEnd.length, helper.initialComments.length);
+  });
+
+  test("there is a comment about a great movie", async () => {
+    const commentsAtEnd = await helper.commentsInDb();
+
+    const contents = commentsAtEnd.map((c) => c.content);
+    assert(contents.includes("This is a great movie"));
+  });
 });
 
-test("there is a correct amount of comments", async () => {
-  const commentsAtEnd = await helper.commentsInDb();
+describe("a user already exists and no comments in db", async () => {
+  beforeEach(async () => {
+    await UserComment.deleteMany({});
 
-  assert.strictEqual(commentsAtEnd.length, helper.initialComments.length);
-});
+    // creating a user
+    await User.deleteMany({});
+    const passwordHash = await getHash("123456");
+    const user = new User({
+      username: helper.initialUser.username,
+      email: helper.initialUser.email,
+      passwordHash,
+    });
+    await user.save();
+    receiverId = user._id.toString();
 
-test("there is a comment about a great movie", async () => {
-  const commentsAtEnd = await helper.commentsInDb();
+    // logging in and getting a token
+    const result = await api
+      .post("/api/login")
+      .send({ username: "commentstester", password: "123456" });
 
-  const contents = commentsAtEnd.map((c) => c.content);
-  assert(contents.includes("This is a great movie"));
-});
+    token = result.body.token;
+  });
 
-test("a valid comment can be added to a user profile by a logged in user", async () => {
-  const newComment = {
-    content: "I am a comment created by a test user",
-  };
+  describe("comment creation", async () => {
+    test("a valid comment can be added to a user profile by a logged in user", async () => {
+      const newComment = {
+        content: "I am a comment created by a test user",
+      };
 
-  await api
-    .post(`/api/comments/profile/${receiverId}`)
-    .set("Authorization", `Bearer ${token}`)
-    .send(newComment)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+      await api
+        .post(`/api/comments/profile/${receiverId}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(newComment)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
 
-  const commentsAtEnd = await helper.commentsInDb();
+      const commentsAtEnd = await helper.commentsInDb();
 
-  const contents = commentsAtEnd.map((c) => c.content);
+      const contents = commentsAtEnd.map((c) => c.content);
 
-  assert.strictEqual(commentsAtEnd.length, helper.initialComments.length + 1);
+      assert.strictEqual(commentsAtEnd.length, 1);
 
-  assert(contents.includes("I am a comment created by a test user"));
-});
+      assert(contents.includes("I am a comment created by a test user"));
+    });
 
-test("a comment by a not logged in user will not be added", async () => {
-  const newComment = {
-    content: "I will not be added",
-  };
+    test("a comment by a not logged in user will not be added", async () => {
+      const newComment = {
+        content: "I will not be added",
+      };
 
-  await api
-    .post(`/api/comments/profile/${receiverId}`)
-    .send(newComment)
-    .expect(401);
+      await api
+        .post(`/api/comments/profile/${receiverId}`)
+        .send(newComment)
+        .expect(401);
 
-  const commentsAtEnd = await helper.commentsInDb();
+      const commentsAtEnd = await helper.commentsInDb();
 
-  const contents = commentsAtEnd.map((c) => c.content);
+      const contents = commentsAtEnd.map((c) => c.content);
 
-  assert.strictEqual(commentsAtEnd.length, helper.initialComments.length);
+      assert.strictEqual(commentsAtEnd.length, 0);
 
-  assert(!contents.includes(newComment.content));
-});
+      assert(!contents.includes(newComment.content));
+    });
 
-test("empty comment is not added to a user profile", async () => {
-  const newComment = {};
+    test("empty comment is not added to a user profile", async () => {
+      const newComment = {};
 
-  await api
-    .post(`/api/comments/profile/${receiverId}`)
-    .set("Authorization", `Bearer ${token}`)
-    .send(newComment)
-    .expect(400);
+      await api
+        .post(`/api/comments/profile/${receiverId}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(newComment)
+        .expect(400);
 
-  const commentsAtEnd = await helper.commentsInDb();
+      const commentsAtEnd = await helper.commentsInDb();
 
-  assert.strictEqual(commentsAtEnd.length, helper.initialComments.length);
-});
+      assert.strictEqual(commentsAtEnd.length, 0);
+    });
+  });
 
-test("a comment can be deleted by its author", async () => {
-  const newComment = {
-    content: "I am not long for this world",
-  };
+  describe("comment deletion", async () => {
+    test("a comment can be deleted by its author", async () => {
+      const newComment = {
+        content: "I am not long for this world",
+      };
 
-  await api
-    .post(`/api/comments/profile/${receiverId}`)
-    .set("Authorization", `Bearer ${token}`)
-    .send(newComment)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+      await api
+        .post(`/api/comments/profile/${receiverId}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(newComment)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
 
-  const commentsAtStart = await helper.commentsInDb();
+      const commentsAtStart = await helper.commentsInDb();
 
-  const commentToDelete = commentsAtStart.find(
-    (comment) => comment.content === "I am not long for this world"
-  );
+      const commentToDelete = commentsAtStart.find(
+        (comment) => comment.content === "I am not long for this world"
+      );
 
-  await api
-    .delete(`/api/comments/profile/${receiverId}/${commentToDelete.id}`)
-    .set("Authorization", `Bearer ${token}`)
-    .send({ authorId: commentToDelete.author.toString() })
-    .expect(204);
+      await api
+        .delete(`/api/comments/profile/${receiverId}/${commentToDelete.id}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send({ authorId: commentToDelete.author.toString() })
+        .expect(204);
 
-  const commentsAtEnd = await helper.commentsInDb();
+      const commentsAtEnd = await helper.commentsInDb();
 
-  const contents = commentsAtEnd.map((c) => c.content);
-  assert(!contents.includes(commentToDelete.content));
+      const contents = commentsAtEnd.map((c) => c.content);
+      assert(!contents.includes(commentToDelete.content));
 
-  assert.strictEqual(commentsAtEnd.length, commentsAtStart.length - 1);
+      assert.strictEqual(commentsAtEnd.length, commentsAtStart.length - 1);
+    });
+
+    test("a comment can not be deleted by another user it this user is not the profile owner", async () => {});
+
+    test("a user can delete a comment on his profile even if he is not the author", async () => {});
+  });
+
+  describe("editing a comment", async () => {
+    test("a profile comment can be edited by its author", async () => {});
+
+    test("a profile comment can not be edited by someone other than its author", async () => {});
+  });
 });
 
 after(async () => {
