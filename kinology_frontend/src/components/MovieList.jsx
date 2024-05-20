0c411431e69267a8c1325534706bd1e89a407148
timmchi <tnmagg@gmail.com>
2024-05-20 20:20:27 +0300

    movielist tests

--- kinology_frontend/src/components/MovieList.jsx
+++ kinology_frontend/src/components/MovieList.jsx
@@ -2,10 +2,10 @@ import MovieCard from "./MovieCard";
 
 const MovieList = ({ movies, onButtonPress, onButtonUnpress, user }) => {
   if (!movies) return <>no movies yet</>;
-  console.log("movies in MovieList", movies);
+  //   console.log("movies in MovieList", movies);
 
   const moviesList = movies;
-  console.log(moviesList);
+  //   console.log(moviesList);
 
   return (
     <div className="listContainer">
@@ -14,7 +14,7 @@ const MovieList = ({ movies, onButtonPress, onButtonUnpress, user }) => {
         {moviesList?.map((movie) => (
           <MovieCard
             movie={movie}
-            key={movie.id}
+            key={movie.id ? movie.id : movie.tmdbId}
             onButtonPress={onButtonPress}
             onButtonUnpress={onButtonUnpress}
             user={user}
---
+++ kinology_frontend/src/components/MovieList.test.jsx
@@ -0,0 +1,37 @@
+import MovieList from "./MovieList";
+import myFavoriteMovies from "../data/myFavoriteMovies";
+import { expect, test } from "vitest";
+import { testSetup } from "../utils/testUtils";
+import { MemoryRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+
+test("renders properly when there are no movies", () => {
+  render(<MovieList />);
+
+  const text = screen.getByText("no movies yet");
+  expect(text).toBeInTheDocument();
+});
+
+test("renders properly with a list of movies", () => {
+  render(
+    <MemoryRouter>
+      <MovieList movies={myFavoriteMovies.slice(0, 3)} />
+    </MemoryRouter>
+  );
+
+  const movie1 = screen.getByText(myFavoriteMovies[0].title);
+  const movie2 = screen.getByText(myFavoriteMovies[1].title);
+  const movie3 = screen.getByText(myFavoriteMovies[2].title);
+
+  expect(movie1).toBeInTheDocument();
+  expect(movie2).toBeInTheDocument();
+  expect(movie3).toBeInTheDocument();
+
+  const movie1Img = screen.getByAltText(`${myFavoriteMovies[0].title} poster`);
+  const movie2Img = screen.getByAltText(`${myFavoriteMovies[1].title} poster`);
+  const movie3Img = screen.getByAltText(`${myFavoriteMovies[2].title} poster`);
+
+  expect(movie1Img).toBeInTheDocument();
+  expect(movie2Img).toBeInTheDocument();
+  expect(movie3Img).toBeInTheDocument();
+});
