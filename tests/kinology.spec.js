8330feb47921ef6866be147463de40c3fd861cba
timmchi <tnmagg@gmail.com>
2024-05-22 17:39:02 +0300

    e2e tests

--- tests/kinology.spec.js
+++ tests/kinology.spec.js
@@ -4,12 +4,39 @@ describe("Kinology", () => {
   test("front page can be opened", async ({ page }) => {
     await page.goto("http://localhost:5173");
 
-    const logoLocator = await page.getByRole("link", { name: "Kinology" });
-    await expect(logoLocator).toBeVisible();
-
-    const welcomeLocator = await page.getByRole("heading", {
+    const welcomeHeader = page.getByRole("heading", {
       name: "Welcome to Kinology",
     });
-    await expect(welcomeLocator).toBeVisible();
+    const welcomeMessage = page.getByText("Choosing a movie made");
+
+    await expect(welcomeMessage).toBeVisible();
+    await expect(welcomeHeader).toBeVisible();
+
+    const registerMessage = page.getByText("Too many good options to");
+
+    await expect(registerMessage).toBeVisible();
+  });
+
+  test("nav bar is visible on the front page", async ({ page }) => {
+    await page.goto("http://localhost:5173");
+
+    const homeLink = page.getByRole("link", { name: "Kinology" });
+    await expect(homeLink).toBeVisible();
+
+    const aboutLink = page.getByRole("link", { name: "About" });
+    await expect(aboutLink).toBeVisible();
+
+    const loginLink = page.getByRole("link", { name: "Log In" });
+    await expect(loginLink).toBeVisible();
+
+    const signupLink = page.getByRole("link", { name: "Sign Up" });
+    await expect(signupLink).toBeVisible();
+  });
+
+  test("registration form can be opened", async ({ page }) => {
+    await page.goto("http://localhost:5173");
+
+    const registrationButton = page.getByRole("button", { name: "Register" });
+    await registrationButton.click();
   });
 });
