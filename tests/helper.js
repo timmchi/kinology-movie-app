cb9ec9e72f7ab82baf005ca42386505be5709d2f
timmchi <tnmagg@gmail.com>
2024-05-24 19:44:10 +0300

    refactoring of e2e tests started

---
+++ tests/helper.js
@@ -0,0 +1,18 @@
+const loginWith = async (page, username, password) => {};
+
+const registerWith = async (
+  page,
+  username,
+  name,
+  email,
+  password,
+  passwordConfirm
+) => {};
+
+const postComment = async (page, content) => {};
+
+const deleteComment = async (page) => {};
+
+const editComment = async (page, content) => {};
+
+export { loginWith, registerWith, postComment, deleteComment, editComment };
