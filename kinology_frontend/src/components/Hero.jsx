3f4603e8e6064328bc56f7e2f4d3f5e41b4e7a90
timmchi <tnmagg@gmail.com>
2024-06-01 12:08:51 +0300

    color change for hero, responsiveness fixes

--- kinology_frontend/src/components/Hero.jsx
+++ kinology_frontend/src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
                     backgroundColor: "#00C9AB",
                     "&:hover": { backgroundColor: "#00755F" },
                     fontSize: 25,
-                    "@media (max-width: 780px)": {
+                    "@media (max-width: 1024px)": {
                       fontSize: 15,
                     },
                   }}
@@ -48,7 +48,7 @@ const Hero = () => {
                     backgroundColor: "#00A660",
                     "&:hover": { backgroundColor: "#00532f" },
                     fontSize: 20,
-                    "@media (max-width: 780px)": {
+                    "@media (max-width: 1024px)": {
                       fontSize: 15,
                     },
                   }}
