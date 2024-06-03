74f6bc27083203249891abc1035a1433222c9468
timmchi <tnmagg@gmail.com>
2024-06-03 14:44:31 +0300

    uibuild

--- kinology_frontend/src/services/contact.js
+++ kinology_frontend/src/services/contact.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/api/contact";
+const baseUrl = "/api/contact";
 
 const sendMessage = async (data) => {
   const response = await axios.post(baseUrl, data);
--- kinology_frontend/src/services/movies.js
+++ kinology_frontend/src/services/movies.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/api/movies";
+const baseUrl = "/api/movies";
 
 const search = async (queryDetails) => {
   console.log("query deatils in movies service in frontend", queryDetails);
