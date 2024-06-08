14dbd568543ea439df6d91b45fcf459cde8193f9
timmchi <tnmagg@gmail.com>
2024-06-08 10:19:24 +0300

    contact api changed to use ethereal in testing, gmail in dev and prod

--- kinology_backend/controllers/contact.js
+++ kinology_backend/controllers/contact.js
@@ -1,8 +1,5 @@
-const nodemailer = require("nodemailer");
-const config = require("../utils/config");
-
+const { sendEmail } = require("../utils/contactUtils");
 const contactRouter = require("express").Router();
-
 const v = require("valibot");
 
 const MessageSchema = v.object({
@@ -20,39 +17,16 @@ const MessageSchema = v.object({
   ]),
 });
 
-// same can be configured for gmail
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.ethereal.email",
-//   port: 587,
-//   secure: false,
-//   auth: {
-//     user: config.ETHEREAL_USER,
-//     pass: config.ETHEREAL_PW,
-//   },
-// });
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: config.GMAIL_USER,
-    pass: config.GMAIL_PW,
-  },
-});
-
 contactRouter.post("/", async (request, response) => {
   const { name, email, message } = request.body;
 
   const parsedMessage = v.parse(MessageSchema, { email, name, message });
 
-  await transporter.sendMail({
-    from: parsedMessage.email,
-    to: config.GMAIL_USER,
-    subject: `Email from ${parsedMessage.name}`,
-    text: `${parsedMessage.message}`,
-    html: `<p>${parsedMessage.name} at ${parsedMessage.email}</p><p>${parsedMessage.message}</p>`,
-  });
+  await sendEmail(
+    parsedMessage.email,
+    parsedMessage.name,
+    parsedMessage.message
+  );
 
   response.status(200).end();
 });
