b37d6f89d2e330c4671b882602f95667a2123149
timmchi <tnmagg@gmail.com>
2024-06-11 19:22:13 +0300

    timekeeping #skip #skipdeployment

--- kinology_frontend/vite.config.js
+++ kinology_frontend/vite.config.js
@@ -12,6 +12,21 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
   test: {
     environment: "jsdom",
     globals: true,
